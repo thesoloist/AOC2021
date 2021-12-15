f53909d10b6a480d84fcc1c869f50db2189231b8
Huaxin Dai <huaxin.dai@outlook.com>
2021-12-14 20:19:49 -0500

    finish day 14 after learning use of caching

--- Day14.py
+++ Day14.py
@@ -1,4 +1,4 @@
-
+import time
 # Online IDE - Code Editor, Compiler, Interpreter
 
 print('Welcome to Online IDE!! Happy Coding :)')
@@ -103,7 +103,7 @@ day14_pair_dict = {
    'VC' : 'S',
    'KV' : 'C',
    'FC' : 'F'}
-# day14_pair_dict = {'CH' : 'B',
+#day14_pair_dict = {'CH' : 'B',
 # 'HH' : 'N',
 # 'CB' : 'H',
 # 'NH' : 'C',
@@ -121,47 +121,92 @@ day14_pair_dict = {
 # 'CN' : 'C'}
 
 day14_input = 'HHKONSOSONSVOFCSCNBC'
-# day14_input = 'NNCB'
+#day14_input = 'NNCB'
 
-run_days = 40
-polymer_dict = {}
+def init_result_dict():
+    out_dict = {}
+    for p in day14_pair_dict.values():
+        if p in out_dict.keys():
+            None
+        else:
+            out_dict[p] = 0
+    return out_dict
 
-def ins_update_dict(l, c, r, day):
-    if day == run_days:
+def ins_update_dict(l, c, r, day, total_days, result_dict):
+    if day == total_days:
         return
     newc_l = day14_pair_dict[l+c]
     newc_r = day14_pair_dict[c+r]
-    if newc_l in polymer_dict.keys():
-        polymer_dict[newc_l] += 1
+    result_dict[newc_l] += 1
+    result_dict[newc_r] += 1
+
+    #print(f"Insert called: day={day} / {total_days} total days, left={l}, center={c}, right={r} -> newc_l = {newc_l}, newc_r = {newc_r}")
+    ins_update_dict(l, newc_l, c, day+1, total_days, result_dict)
+    ins_update_dict(c, newc_r, r, day+1, total_days, result_dict)
+
+def ins_update_dict_cached(l, r, day, cached_days, total_days, cached_dict, result_dict):
+    if day == total_days:
+        return
     else:
-        polymer_dict[newc_l] = 1
-    if newc_r in polymer_dict.keys():
-        polymer_dict[newc_r] += 1
-    else:
-        polymer_dict[newc_r] = 1
-    # print(f"Insert called: day={day}, left={l}, center={c}, right={r} -> newc_l = {newc_l}, newc_r = {newc_r}")
-    ins_update_dict(l, newc_l, c, day+1)
-    ins_update_dict(c, newc_r, r, day+1)
+        if day == total_days - cached_days:
+            #print(f"CACHED - Using cached value: day={day}, pair = {l, r}")
+            for p in result_dict.keys():
+                result_dict[p] += cached_dict[l+r][p]
+        else:
+            newc = day14_pair_dict[l + r]
+            result_dict[newc] += 1
+
+            #print(f"CACHED - Insert called: day={day}, left={l}, center={newc}, right={r}")
+            ins_update_dict_cached(l, newc, day + 1, cached_days, total_days, cached_dict, result_dict)
+            ins_update_dict_cached(newc, r, day + 1, cached_days, total_days, cached_dict, result_dict)
+
+def build_cache(pair,days):
+    # print(f"Building cache for pair {pair}, cache for {days} days")
+    out_result_dict = init_result_dict()
+    # out_result_dict[pair[0]] += 1
+    # out_result_dict[pair[1]] += 1
+    out_result_dict[day14_pair_dict[pair]] += 1
+
+    ins_update_dict(pair[0], day14_pair_dict[pair], pair[1], 1, days, out_result_dict)
+    return out_result_dict
+
+
+
+start_time = time.time()
+
+run_days = 40
+polymer_dict = {}
+# initialize output dict
+polymer_dict = init_result_dict()
+
+using_cache = True
+cache_days = 20
+cache_dict = {}
+
+if using_cache is True and cache_days > 0:
+    for p in day14_pair_dict.keys():
+        cache_dict[p] = build_cache(p, cache_days)
+    print(f"cache dict: {cache_dict}")
 
 for p_idx in range(len(day14_input)):
-    if day14_input[p_idx] in polymer_dict:
-        polymer_dict[day14_input[p_idx]] += 1
-    else:
-        polymer_dict[day14_input[p_idx]] = 1
+    polymer_dict[day14_input[p_idx]] += 1
+
     if p_idx < len(day14_input)-1:
         pair = day14_input[p_idx] + day14_input[p_idx+1]
         insert_c = day14_pair_dict[pair]
-        print(f"Got pair {pair} -> inserting {insert_c}")
-        if insert_c in polymer_dict:
+        print(f"time {time.time() - start_time} - Got pair {pair} -> inserting {insert_c}")
+        if using_cache is False:
             polymer_dict[insert_c] += 1
+            ins_update_dict(day14_input[p_idx], insert_c, day14_input[p_idx+1], 1, run_days, polymer_dict)
         else:
-            polymer_dict[insert_c] = 1
-        ins_update_dict(day14_input[p_idx], insert_c, day14_input[p_idx+1], 1)
-    
+            ins_update_dict_cached(day14_input[p_idx], day14_input[p_idx+1], 0, cache_days, run_days, cache_dict, polymer_dict)
+
+print(f"Finished in {time.time() - start_time} seconds")
+
 max_cnt, max_itm = 0, ''
 min_cnt, min_itm = max(polymer_dict.values()), ''
 
-print(polymer_dict)
+print(f"result dict: {polymer_dict}")
 
 for k in polymer_dict.keys():
     if polymer_dict[k] > max_cnt:
